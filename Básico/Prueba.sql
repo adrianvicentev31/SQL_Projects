931382f7906d59dc5d43f63ba339c6d06d394047
Adrian Vicente Vega <91351079+adrianvicentev31@users.noreply.github.com>
2021-10-02 21:25:27 -0500

    Update and rename Prueba.txt to Prueba.sql

---
+++ Básico/Prueba.sql
@@ -0,0 +1,35 @@
+/* SQL: Lenguaje de Consulta Estructurado */
+/* NoSQL: Lenguaje de Consulta No Estructurado */
+/* SEQUEL: Structured English Query Language */
+
+
+/* DML: Lenguaje de Manipulación de Datos (Select, Insert, Update, Delate) */
+/* DDL: Lenguaje de Definición de Datos (Create, Alter, Drop) */
+
+/* 
+Otros objetos:
+Triggers
+Relaciones
+Datos personalizados
+*/
+
+------------------------------------------------------------------------------------
+/* Clase 1 */
+------------------------------------------------------------------------------------
+
+/* Uso de la DataBase */
+USE TSQL
+GO
+
+/* Consulta general de las tablas */
+SELECT * FROM Nums
+SELECT * FROM HR.Employees
+SELECT * FROM Production.Categories
+SELECT * FROM Production.Products
+SELECT * FROM Production.Suppliers
+SELECT * FROM Sales.Customers
+SELECT * FROM Sales.OrderDetails
+SELECT * FROM Sales.Orders
+SELECT * FROM Sales.Shippers
+SELECT * FROM Stats.Scores
+SELECT * FROM Stats.Tests
